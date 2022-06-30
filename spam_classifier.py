ff8ec8f0319e0d91f12f1bbf7d5f2248b8e33ad8
sidlami <72751704+sidlami@users.noreply.github.com>
2022-06-30 15:39:28 +0200

    roadmap for feedforward neural net in comments

--- spam_classifier.py
+++ spam_classifier.py
@@ -1,5 +1,4 @@
 # to do: 
-#task 2: preprocessing in this file
 #continue wth task 3: free neural net
 #task 4: in a clean way and in its on py file
 #task 1 + write report
@@ -15,24 +14,30 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 # import data
 corpus = pd.read_csv("data/SMSSpamCollection", sep="\t", names=["label", "message"])
 
-# task 2: pre-processing:
-# remove stop words, punctuations and white spaces as well as normalise tokens by making all tokens lower case
+# task 2: pre-processing: 
+# tokenization --> done by default through param token_pattern during init of vectorizer object
+# remove stop words, --> done by setting param stop_words to english during init of vectorizer object. Alternatively, param max_df can be set to a value in the range [0.7, 1.0) to automatically detect and filter stop words based on intra corpus document frequency of terms.
+# punctuations --> done by default set up of param token_pattern during init of vectorizer object
+# and white spaces as well as --> done by default set up of param token_pattern during init of vectorizer object
+# normalise tokens by making all tokens lower case --> done by setting param lowercase to true during init of vectorizer object
+# (source: https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html)
 
 # create train and test set
 x_train, x_test, y_train, y_test = train_test_split(corpus.message, corpus.label, test_size=0.2, random_state=53)
 
 # set up vectorizers
 ## ... using bag of words --> requires following pre-processing according to lecture: Lowercasing, Punctuation removal, Word tokenization, Stop word removal
-bow_vectorizer = CountVectorizer(stop_words='english') # init CountVectorizer object with set up to erase stopwords
+## note: default token pattern (= r”(?u)\b\w\w+\b”) removes punctuations 
+bow_vectorizer = CountVectorizer(stop_words='english', lowercase=True) # init CountVectorizer object with set up to erase stopwords
 
 ## ... using tfidf --> requires following pre-processing according to lecture: Lowercasing, Punctuation removal, Word tokenization, Stop word removal
-tfidf_vectorizer = TfidfVectorizer(lowercase=True, stop_words="english") # add eventually param: max_df=0.7 --> this erases additional stop words or words that occure to often accross all documents
+tfidf_vectorizer = TfidfVectorizer(stop_words="english", lowercase=True) # add eventually param: max_df=0.7 --> this erases additional stop words or words that occure to often accross all documents
 
 ## ... using bag of N-Gram (n=2) --> requires following pre-processing according to lecture:  Lowercasing, Punctuation removal, Word tokenization
-bigram_vectorizer = CountVectorizer(ngram_range=(2, 2))
+bigram_vectorizer = CountVectorizer(ngram_range=(2, 2), lowercase=True)
 
 ## ... using bag of N-Gram (n=1-2) 
-one2twoGram_vectorizer = CountVectorizer(ngram_range=(1, 2))
+one2twoGram_vectorizer = CountVectorizer(ngram_range=(1, 2), lowercase=True)
 
 ## summarize vectorizers
 vectorizers = [
@@ -66,6 +71,8 @@ for vectorizer in vectorizers:
     print("confusion matrix (spam-ham):", cm)
     print("\n")
 
-# free forward neural net
-# to do google: classification with free forward neural net
+# spam classifier based on a feed forward neural network
 # follow this tutorial: https://towardsdatascience.com/feed-forward-neural-networks-how-to-successfully-build-them-in-python-74503409d99a
+# use sklearn's MLPClassifier to build feedforward neural net: https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
+# example of how to use sklearn MLPClassifier object: https://scikit-learn.org/stable/modules/neural_networks_supervised.html
+
