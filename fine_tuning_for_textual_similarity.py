8c2971fbee2fea31cced17ed327e3c8bc65bd44e
sid030sid <78762408+sid030sid@users.noreply.github.com>
2022-12-03 12:35:05 +0100

    Delete fine_tuning_for_textual_similarity.py

--- fine_tuning_for_textual_similarity.py
+++
@@ -1,5 +0,0 @@
-# https://www.youtube.com/watch?v=D-BlhDFXt30
-
-#https://www.google.com/search?q=fine+tune+bert+for+text+similarity&rlz=1C1PNBB_enDE920DE920&oq=fine+tuning+bert+text+sim&aqs=chrome.1.69i57j0i22i30.12146j1j7&sourceid=chrome&ie=UTF-8
-
-# https://medium.com/the-artificial-impostor/finetuning-bert-for-similarity-search-59d1a0bbaf1
